c73e4732912878d5afd6b8af5b34e7a973b994af
Anton <merkulovich166@gmail.com>
2025-07-15 23:12:47 +0300

    dev

---
+++ src/js/components/news-slider.js
@@ -0,0 +1,25 @@
+(function () {
+	const slider = document.querySelector(".news-slider__slider");
+
+	if (!slider) return;
+
+	const sliderParams = {
+		spaceBetween: 16,
+		slidesPerView: 2,
+		slidesPerGroup: 2,
+		speed: 600,
+		mousewheel: {
+			forceToAxis: true,
+		},
+		navigation: {
+			prevEl: ".news-slider__nav-button--prev",
+			nextEl: ".news-slider__nav-button--next",
+		},
+		keyboard: {
+			enabled: true,
+			onlyInViewport: true,
+		},
+	};
+
+	new Swiper(slider, sliderParams);
+})();
